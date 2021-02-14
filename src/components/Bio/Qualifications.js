e1f43b89b8b4ed3fa72553152fd1fda8a7ef1896
Mari Taslagyan <32289706+mtaslagyan@users.noreply.github.com>
2021-02-13 20:48:26 -0800

    Update Qualifications.js

--- src/components/Bio/Qualifications.js
+++ src/components/Bio/Qualifications.js
@@ -9,7 +9,7 @@ const Bio_Home = () => {
       <div className="container-fluid mx-0 px-0 Bio-banner">
         <div className="row">
           <div className="col-md-7 col-12 Bio-content">
-            <h1 className="Bio-title text-center">Qualifications</h1>
+            <h1 className="Bio-title text-left">Qualifications <img src={Uspta} alt="Criss Rodriguez Tennis USPTA Certification"/></h1>
               {/* <span className="SP-intro">Each lesson is tailored based on student’s needs, age, level, and goals. Nothing fills me up more than seeing my students excel and achieve their goals.</span> */}
               {/* <div className="Bio-intro text-center">With over 8-years of coaching experience and over 20-years of competitive playing experience, I have been in the tennis world from birth. Born to two tennis coaching parents, I have been around tennis my entire life. 
               </div> */}
@@ -17,7 +17,6 @@ const Bio_Home = () => {
                 <div className="col-0 col-lg-1"></div>
                 <div className="col-6 col-lg-5">
                   <ul className="qualifications">
-                    <li><img src={Uspta}/></li>
                     <li>USPTA Certified</li>
                     <li>8+ years coaching experience</li>
                     <li>“10 and under tennis” Certified</li>
