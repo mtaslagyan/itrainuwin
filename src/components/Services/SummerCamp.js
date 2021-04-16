2a5a87da948eaa6189948469fb392b7c24303a3f
Mari Taslagyan <32289706+mtaslagyan@users.noreply.github.com>
2021-04-16 00:19:53 -0700

    Update SummerCamp.js

--- src/components/Services/SummerCamp.js
+++ src/components/Services/SummerCamp.js
@@ -9,16 +9,16 @@ class SummerCamp extends Component {
       return (
         <div className="summercamp">
           <div className="container">
-            <div className="row summercamp-flyers">
-              <div className="col-12 col-lg-6 text-center"><a target="_blank" href="https://forms.gle/C8UAGW1CA2wL21K19"><img className="imageflyer1" src={Flyer1} alt="Summer Tennis Camp for kids/juniors at Calabasas High SChool run by Criss and Josh"/></a></div>
-              <div className="col-12 col-lg-6 text-center mt-5 mt-lg-0"><a target="_blank" href="https://forms.gle/C8UAGW1CA2wL21K19"><img className="imageflyer2" src={Flyer2} alt="Summer Tennis Camp for kids/juniors at Calabasas High SChool run by Criss and Josh"/></a></div>
-            </div>
-            <div className="row mt-5">
+          <div className="row">
                 <div className="col-12 text-center">
                  <div className="banner-btnWrap text-center">
-                    <a href="https://forms.gle/C8UAGW1CA2wL21K19" target="_blank" className="SP-requestBtn">Register For Camp</a>
+                    <a href="https://forms.gle/C8UAGW1CA2wL21K19" target="_blank" className="SP-requestBtn">Register Now!</a>
                   </div>
                 </div>
+            </div>
+            <div className="row mt-5 summercamp-flyers">
+              <div className="col-12 col-lg-6 text-center"><a target="_blank" href="https://forms.gle/C8UAGW1CA2wL21K19"><img className="imageflyer1" src={Flyer1} alt="Summer Tennis Camp for kids/juniors at Calabasas High SChool run by Criss and Josh"/></a></div>
+              <div className="col-12 col-lg-6 text-center mt-5 mt-lg-0"><a target="_blank" href="https://forms.gle/C8UAGW1CA2wL21K19"><img className="imageflyer2" src={Flyer2} alt="Summer Tennis Camp for kids/juniors at Calabasas High SChool run by Criss and Josh"/></a></div>
             </div>
           </div>
         </div>
